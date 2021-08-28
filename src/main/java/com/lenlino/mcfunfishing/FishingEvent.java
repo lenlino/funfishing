98f00e01dda07782b01d9c63c57c4c15edf9fdae
koufu193 <toufu335@gmail.com>
2021-08-28 13:55:41 +0900

    aaa

---
+++ src/main/java/com/lenlino/mcfunfishing/FishingEvent.java
@@ -0,0 +1,10 @@
+package com.lenlino.mcfunfishing;
+
+import org.bukkit.event.Listener;
+
+public class FishingEvent implements Listener {
+    Mcfunfishing plugin;
+    public FishingEvent(Mcfunfishing plugin){
+        this.plugin=plugin;
+    }
+}
--- src/main/java/com/lenlino/mcfunfishing/Mcfunfishing.java
+++ src/main/java/com/lenlino/mcfunfishing/Mcfunfishing.java
@@ -1,17 +1,17 @@
 package com.lenlino.mcfunfishing;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Mcfunfishing extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(new FishingEvent(this),this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        saveConfig();
     }
 }
