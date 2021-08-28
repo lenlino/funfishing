88064912731eec6e30fcd765ef8ccff83a10a668
koufu193 <toufu335@gmail.com>
2021-08-28 15:06:11 +0900

    aaa

--- src/main/java/com/lenlino/mcfunfishing/Mcfunfishing.java
+++ src/main/java/com/lenlino/mcfunfishing/Mcfunfishing.java
@@ -1,14 +1,25 @@
 package com.lenlino.mcfunfishing;
 
 import org.bukkit.Bukkit;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Mcfunfishing extends JavaPlugin {
+    List<Fish> fish=new ArrayList<>();
+    FileConfiguration config=getConfig();
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new FishingEvent(this),this);
+        config.options().copyDefaults(true);
+        for(String key:config.getKeys(false)){
+            fish.add(new Fish(config.getInt(key+".min"),config.getInt(key+".max"),config.getString(key+".name"), Material.valueOf(config.getString(key+".material")),config.getString(key+".rarity")));
+        }
     }
-
     @Override
     public void onDisable() {
         saveConfig();
