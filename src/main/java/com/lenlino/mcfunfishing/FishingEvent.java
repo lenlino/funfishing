5092953035d5c82f9cdb98168dc4f45fd48b0a1e
koufu193 <toufu335@gmail.com>
2021-08-28 17:35:27 +0900

    aaa

--- src/main/java/com/lenlino/mcfunfishing/FishingEvent.java
+++ src/main/java/com/lenlino/mcfunfishing/FishingEvent.java
@@ -1,11 +1,16 @@
 package com.lenlino.mcfunfishing;
 
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+
+import java.util.Random;
 
 public class FishingEvent implements Listener {
     Mcfunfishing plugin;
+    Random random=new Random();
     public FishingEvent(Mcfunfishing plugin){
         this.plugin=plugin;
     }
@@ -15,6 +20,16 @@ public class FishingEvent implements Listener {
             if(e.getItem().hasItemMeta()) {
                 if (plugin.FishKouka.containsKey(e.getItem().getItemMeta().getDisplayName())) {
                     plugin.FishKouka.get(e.getItem().getItemMeta().getDisplayName()).Eatfish(e.getPlayer());
+                }
+            }
+        }
+    }
+    @EventHandler
+    public void GetFishEvent(PlayerFishEvent e){
+        if(e.getState()== PlayerFishEvent.State.CAUGHT_FISH){
+            if(e.getCaught()!=null){
+                if(e.getCaught() instanceof Item&&random.nextBoolean()){
+                    ((Item)e.getCaught()).setItemStack(plugin.fish.get(random.nextInt(plugin.fish.size())).getFish());
                 }
             }
         }
