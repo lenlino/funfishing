62374665b7533184445db27623b15d5a91633b40
koufu193 <toufu335@gmail.com>
2021-08-29 11:08:49 +0900

    aaa

--- src/main/java/com/lenlino/mcfunfishing/Mcfunfishing.java
+++ src/main/java/com/lenlino/mcfunfishing/Mcfunfishing.java
@@ -8,8 +8,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
